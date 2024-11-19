f6a82d022803e6a6a34c7c69d4b3ae1bc6ea2b6e
Jeremy Irilli <jeremy.irilli@student.uibk.ac.at>
2024-11-19 08:43:38 +0100

    action.pl

--- action.pl
+++ action.pl
@@ -13,6 +13,45 @@ is_prolog_file(File) :-
 run_tests_for_file(Dir, File) :-
     file_name_extension(ModuleName, _, File),
     atomic_list_concat([Dir, '/', File], FilePath),
+     update_file(FilePath, replace(ModuleName, _, _), true).
     load_files(FilePath, [silent(true)]),
     call(ModuleName),
+     update_file(FilePath, replace(ModuleName, _, _), false).
    
+replace(test_interval, A, B) :-
+    A = ':- use_module(library(interval)).',
+    B = ':- use_module(''prolog/interval'').'.
+
+replace(test_interval, A, B) :-
+    A = ':- use_module(library(mcclass)).',
+    B = ':- use_module(''prolog/mcclass'').'.
+
+replace(test_interval, A, B) :-
+    A = ':- use_module(library(rint)).',
+    B = ':- use_module(''prolog/rint'').'.
+
+update_file(FilePath, ReplacePredicate, ApplyReplace) :-
+    setup_call_cleanup(
+        open(FilePath, read, In),
+        read_string(In, _, Content),
+        close(In)
+    ),
+    split_string(Content, "\n", "", Lines),
+    maplist(apply_replace(ReplacePredicate, ApplyReplace), Lines, NewLines),
+    atomic_list_concat(NewLines, "\n", NewContent),
+    setup_call_cleanup(
+        open(FilePath, write, Out),
+        write(Out, NewContent),
+        close(Out)
+    ).
+
+
+apply_replace(ReplacePredicate, true, Line, NewLine) :-
+    (   call(ReplacePredicate, Line, NewLine)
+    ->  true
+    ;   NewLine = Line).
+
+apply_replace(ReplacePredicate, false, Line, NewLine) :-
+    (   call(ReplacePredicate, NewLine, Line)
+    ->  true
+    ;   NewLine = Line).
